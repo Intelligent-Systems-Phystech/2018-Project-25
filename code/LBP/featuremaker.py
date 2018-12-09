af6db51805700406e287dbb119e32b9257603e80
Ivan-Razumov <razzumoff@gmail.com>
2018-12-09 22:52:50 +0300

    Try to make learning plot

--- code/LBP/featuremaker.py
+++ code/LBP/featuremaker.py
@@ -8,29 +8,14 @@ des = DESCRIPTOR()
 cell_lbp_hist = des.cell_lbp_hist
 hog_hist = des.hog_hist
 
-def makehist_positive(stat): #stat = test or train
-    par = Parser(stat)
-    pdims = par.parse('INRIAPerson')
-    ret = []
-    for key in pdims.keys():
-        cords = pdims[key]
-        image = cv.imread('INRIAPerson/'+key, 1)
-        for cns in cords:
-            pimg = image[cns[0][1]:cns[1][1], cns[0][0]:cns[1][0]]
-            pimg = cv.resize(pimg, (64,128), interpolation=cv.INTER_LINEAR)
-            hist = hog_hist(pimg)
-            ret.append(hist)
-            #print len(hist)
-            #cv.imshow('wnd', pimg)
-            #cv.waitKey()
-            #cv.destroyAllWindows()
-    for h in ret:
-        h.reshape(3780)
-    return ret
-
-def makehist_positive_norm(path, dsc = 'HOG+LBP'):
+def makehist_positive_norm(stat, dsc = 'HOG+LBP', number = 500):
+    if stat == 'train':
+        path = 'INRIAPerson/train_64x128_H96'
+    if stat == 'test':
+        path = 'INRIAPerson/test_64x128_H96'
     poslist = open(path + '/' + 'pos.lst', 'r')
     ret = []
+    k = 0
     for imname in poslist:
         imname = re.split(r'/', imname[:-1])[2]
         imname = path + '/pos/' + imname
@@ -44,13 +29,16 @@ def makehist_positive_norm(path, dsc = 'HOG+LBP'):
         if dsc == 'LBP':
             hist = des.cell_lbp_hist(image)
         ret.append(hist)
-
+        k += 1
+        if k == number:
+            return ret
     return ret
 
-def makehist_negative(stat, dsc = 'HOG+LBP'):
+def makehist_negative(stat, dsc = 'HOG+LBP', number = 500):
     neglstfname = 'INRIAPerson/'+ stat +'/neg.lst'
     neglist = open(neglstfname, 'r')
     ret = []
+    k = 0
     for negname in neglist:
         negname = 'INRIAPerson/' + negname[:-1]
         image = cv.imread('INRIAPerson/train_64x128_H96/pos/crop_000010a.png', 0)
@@ -74,7 +62,28 @@ def makehist_negative(stat, dsc = 'HOG+LBP'):
             left += nfeatures
             right += nfeatures
             #print hst.shape
+            k += 1
+            if k == number:
+                return ret
     return ret
 
-
-
+# this is for sliding windows for industriad detector
+def makehist_positive(stat): #stat = test or train
+    par = Parser(stat)
+    pdims = par.parse('INRIAPerson')
+    ret = []
+    for key in pdims.keys():
+        cords = pdims[key]
+        image = cv.imread('INRIAPerson/'+key, 1)
+        for cns in cords:
+            pimg = image[cns[0][1]:cns[1][1], cns[0][0]:cns[1][0]]
+            pimg = cv.resize(pimg, (64,128), interpolation=cv.INTER_LINEAR)
+            hist = hog_hist(pimg)
+            ret.append(hist)
+            #print len(hist)
+            #cv.imshow('wnd', pimg)
+            #cv.waitKey()
+            #cv.destroyAllWindows()
+    for h in ret:
+        h.reshape(3780)
+    return ret
